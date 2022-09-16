16dc45822637c50c5c9a8d6c1a0c13af3ab5cc13
Violet Hayes <ampc11234@gmail.com>
2022-09-16 08:11:42 -0700

    Created page layout and added navbar

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [svelte(), tailwind()],
-  site: "https://thegreatviolet.github.io",
+  site: "https://violeth.tech",
   base: "/thegreatviolet"
 });
---
+++ src/components/Navbar.svelte
@@ -0,0 +1,13 @@
+<div
+  class="h-12 w-full p-2
+  flex flex-row align-middle
+border-gray-900 border-b-2"
+>
+  <a href="/"><p class="navbar-item">Home</p></a>
+</div>
+
+<style>
+  .navbar-item {
+    @apply text-gray-200 font-bold hover:text-gray-400 ease-in-out duration-200;
+  }
+</style>
