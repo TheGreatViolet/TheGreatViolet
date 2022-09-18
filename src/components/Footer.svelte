49a88faac545021652f5d59475f708933b88b2bf
Violet Hayes <ampc11234@gmail.com>
2022-09-18 12:26:20 -0700

    Created aboutsite page

--- src/components/Footer.svelte
+++ src/components/Footer.svelte
@@ -1,10 +1,12 @@
-<div class="w-full content-center text-center">
+<div class="w-full content-center text-center flex-row space-x-4">
   <a
     href="https://github.com/TheGreatViolet/TheGreatViolet"
     class="footer-item"
   >
     Source Code
   </a>
+
+  <a href="/aboutsite" class="footer-item"> About This Site </a>
 </div>
 
 <style>
