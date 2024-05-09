60a6a1c45a323736774fa48d3d2f264e661366db
Violet Hayes <ampc11234@gmail.com>
2024-05-09 15:08:17 -0700

    uhh maybe this fixes

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,5 +6,5 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [svelte(), tailwind()],
-  site: "https://thegreatviolet.github.io/thegreatviolet",
+  site: "https://thegreatviolet.github.io/",
 });
